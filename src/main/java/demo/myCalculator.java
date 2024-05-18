e6d07f97c224a0d20b9bb00aa36ed49efa1a1289
Jivesh Singh <Singhjivesh57@gmail.com>
2024-05-18 14:34:26 +0530

    mul func added

--- src/main/java/demo/myCalculator.java
+++ src/main/java/demo/myCalculator.java
@@ -7,12 +7,16 @@ public class myCalculator {
          public int diff(int a, int b) {
         	 return a-b;
          }
+         public int mul(int a, int b) {
+        	 return a*b;
+         }
            	
 	
 	public static void main(String[] args) {
 		myCalculator calc = new myCalculator();
 		System.out.println("sum is :" +calc.sum(20, 10));
 		System.out.println("diff is :" +calc.diff(20, 10));
+		System.out.println("mul is :" +calc.mul(20, 10));
 
 	}
 
